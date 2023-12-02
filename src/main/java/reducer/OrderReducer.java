0b9886c35286f1c899804e1ec6400a04e0634be8
Aurora-Veil <m18962298089@163.com>
2023-12-02 22:19:27 +0800

    First Try

--- src/main/java/reducer/OrderReducer.java
+++ src/main/java/reducer/OrderReducer.java
@@ -1,4 +1,42 @@
 package reducer;
 
-public class OrderReducer {
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
+
+import java.io.IOException;
+
+public class OrderReducer extends Reducer<Text, Text, NullWritable, Text> {
+
+    private MultipleOutputs<NullWritable, Text> multipleOutputs;
+
+    @Override
+    protected void setup(Context context) {
+        multipleOutputs = new MultipleOutputs<>(context);
+    }
+
+
+    @Override
+    protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        for (Text value: values) {
+            String OrderType = value.toString().split("\t")[4];
+            switch (OrderType){
+                case "1":
+                    multipleOutputs.write("MarketOrder", NullWritable.get(), value);
+                    break;
+                case "2":
+                    multipleOutputs.write("LimitOrder", NullWritable.get(), value);
+                    break;
+                case "U":
+                    multipleOutputs.write("SpecOrder", NullWritable.get(), value);
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void cleanup(Context context) throws IOException, InterruptedException {
+        multipleOutputs.close();
+    }
 }
